a8b55f3134ad3f447934046b5c76488bcd8e650c
Dominic Gerth <d.gerth1990@googlemail.com>
2021-12-10 12:46:52 +0100

    addet more questions and changed the scale of choice

--- questions.js
+++ questions.js
@@ -1,114 +1,135 @@
 let questions = [
     html = [
-    {
-        "question": "Wer hat HTML Erfunden?",
-        "answer_1": "Robbie Williams.",
-        "answer_2": "Lady Gaga.",
-        "answer_3": "Tim Berners-Lee.",
-        "answer_4": "Justin Bieber.",
-        "right_answer": 3
-    },
-    {
-        "question": "Wofür ist HTML die Abkürzung?",
-        "answer_1": "High Tech Master Language.",
-        "answer_2": "HyperText Markup Language.",
-        "answer_3": "HyperText Makeup Language.",
-        "answer_4": "Hyundai Tesla Mazda Lambo",
-        "right_answer": 2
-    }, {
-        "question": "Wofür wird das &ltb> - Tag verwendet?",
-        "answer_1": "Dieses Tag gibt es nicht.",
-        "answer_2": "Mit dem &ltb> - Tag markiert man Code der nicht Mitgelesen werden soll.",
-        "answer_3": "Um HTML Text braun zu färben.",
-        "answer_4": "Um HTML Text fett zu drucken.",
-        "right_answer": 4
-    }, {
-        "question": "Müssen alle Tags geschlossen werden?",
-        "answer_1": "Es gibt auch selbstschließende Tags.",
-        "answer_2": "Ja!",
-        "answer_3": "Warum sollte ich alles Tagsüber schließen?",
-        "answer_4": "Tags müssen generell nicht geschlossen werden.",
-        "right_answer": 1
-    }, {
-        "question": "Wieviel verschiedene h-Tags gibt es?",
-        "answer_1": "3",
-        "answer_2": "1",
-        "answer_3": "6",
-        "answer_4": "9",
-        "right_answer": 3
-    }, {
-        "question": "Mit welchem Tag wird ein Link bestimmt?",
-        "answer_1": "&lti>",
-        "answer_2": "&lta>",
-        "answer_3": "&ltb>",
-        "answer_4": "&ltg>",
-        "right_answer": 2
-    }],
+        {
+            "question": "Wer hat HTML Erfunden?",
+            "answer_1": "Robbie Williams.",
+            "answer_2": "Lady Gaga.",
+            "answer_3": "Tim Berners-Lee.",
+            "answer_4": "Justin Bieber.",
+            "right_answer": 3
+        },
+        {
+            "question": "Wofür ist HTML die Abkürzung?",
+            "answer_1": "High Tech Master Language.",
+            "answer_2": "HyperText Markup Language.",
+            "answer_3": "HyperText Makeup Language.",
+            "answer_4": "Hyundai Tesla Mazda Lambo",
+            "right_answer": 2
+        }, {
+            "question": "Wofür wird das &ltb> - Tag verwendet?",
+            "answer_1": "Dieses Tag gibt es nicht.",
+            "answer_2": "Mit dem &ltb> - Tag markiert man Code der nicht Mitgelesen werden soll.",
+            "answer_3": "Um HTML Text braun zu färben.",
+            "answer_4": "Um HTML Text fett zu drucken.",
+            "right_answer": 4
+        }, {
+            "question": "Müssen alle Tags geschlossen werden?",
+            "answer_1": "Es gibt auch selbstschließende Tags.",
+            "answer_2": "Ja!",
+            "answer_3": "Warum sollte ich alles Tagsüber schließen?",
+            "answer_4": "Tags müssen generell nicht geschlossen werden.",
+            "right_answer": 1
+        }, {
+            "question": "Wieviel verschiedene h-Tags gibt es?",
+            "answer_1": "3",
+            "answer_2": "1",
+            "answer_3": "6",
+            "answer_4": "9",
+            "right_answer": 3
+        }, {
+            "question": "Mit welchem Tag wird ein Link bestimmt?",
+            "answer_1": "&lti>",
+            "answer_2": "&lta>",
+            "answer_3": "&ltb>",
+            "answer_4": "&ltg>",
+            "right_answer": 2
+        }],
     css = [
         {
-            "question": "Mit welchem Tag wird ein Link bestimmt?",
-            "answer_1": "&lti>",
-            "answer_2": "&lta>",
-            "answer_3": "&ltb>",
-            "answer_4": "&ltg>",
-            "right_answer": 2
+            "question": "Wofür steht CSS?",
+            "answer_1": "Centered Style Sheet",
+            "answer_2": "Cyber Style Speaking",
+            "answer_3": "Cascading Style Sheets",
+            "answer_4": "Cascading Scale Style",
+            "right_answer": 3
         },
         {
-            "question": "Mit welchem Tag wird ein Link bestimmt?",
-            "answer_1": "&lti>",
-            "answer_2": "&lta>",
-            "answer_3": "&ltb>",
-            "answer_4": "&ltg>",
-            "right_answer": 2
+            "question": "Wofür wird CSS verwendet?",
+            "answer_1": "CSS bestimmt, wie Elemente von Markupsprachen gerendert werden.",
+            "answer_2": "Mit CSS bekommen Websites logik",
+            "answer_3": "CSS wird heutzutage nicht mehr verwendet",
+            "answer_4": "Mit CSS werden die Datenbanken verwaltet",
+            "right_answer": 1
         },
         {
-            "question": "Mit welchem Tag wird ein Link bestimmt?",
-            "answer_1": "&lti>",
-            "answer_2": "&lta>",
-            "answer_3": "&ltb>",
-            "answer_4": "&ltg>",
-            "right_answer": 2
+            "question": "Wie bindet man eine CSS Datei ein?",
+            "answer_1": "Per Drag and Drop",
+            "answer_2": "Mit dem i Tag",
+            "answer_3": "Es reicht, wenn die CSS Datei auf dem Server liegt",
+            "answer_4": "Mit dem link-Tag",
+            "right_answer": 4
         },
         {
-            "question": "Mit welchem Tag wird ein Link bestimmt?",
-            "answer_1": "&lti>",
-            "answer_2": "&lta>",
-            "answer_3": "&ltb>",
-            "answer_4": "&ltg>",
+            "question": "Wieviele CSS-Versionen gibt es?",
+            "answer_1": "2",
+            "answer_2": "5",
+            "answer_3": "6",
+            "answer_4": "4",
             "right_answer": 2
+        }, {
+            "question": "Was ist Bootstrap?",
+            "answer_1": "Es ist ein Backend-Web-Framework",
+            "answer_2": "Es dient der Verwaltung von Nutzerdaten",
+            "answer_3": "Es ist ein Frontend-Web-Framework",
+            "answer_4": "Es wird für KI entwicklung benötigt",
+            "right_answer": 3
         }
     ],
     js = [
         {
-            "question": "Mit welchem Tag wird ein Link bestimmt?",
-            "answer_1": "&lti>",
-            "answer_2": "&lta>",
-            "answer_3": "&ltb>",
-            "answer_4": "&ltg>",
+            "question": "Wer ist der Erfinder von Java Script?",
+            "answer_1": "Rudolph Norris",
+            "answer_2": "Brendan Eich",
+            "answer_3": "Chuck Norris",
+            "answer_4": "William Strauss",
             "right_answer": 2
         },
         {
-            "question": "Mit welchem Tag wird ein Link bestimmt?",
-            "answer_1": "&lti>",
-            "answer_2": "&lta>",
-            "answer_3": "&ltb>",
-            "answer_4": "&ltg>",
-            "right_answer": 2
+            "question": "Was ist eine Boolean expression?",
+            "answer_1": "Ein anderes Wort für Mobbing",
+            "answer_2": "Eine verletzung von Programmierregeln",
+            "answer_3": "Eine Verallgemeinerung der logischen Operatoren",
+            "answer_4": "Damit wird ein Wert einer Variablen zugewiesen",
+            "right_answer": 3 
         },
         {
-            "question": "Mit welchem Tag wird ein Link bestimmt?",
-            "answer_1": "&lti>",
-            "answer_2": "&lta>",
-            "answer_3": "&ltb>",
-            "answer_4": "&ltg>",
-            "right_answer": 2
+            "question": "Was ist eine for-Schleife?",
+            "answer_1": "Eine Kontrollstruktur, um zu überprüfen, ob eine Aussage richtig oer falsch ist",
+            "answer_2": "Ein Leitfaden für Clean Coding",
+            "answer_3": "Ein erfundener Begriff",
+            "answer_4": "Eine Kontrollstruktur mit der man Anweisungen mit einer bestimmten Anzahl von Wiederholungen ausführen kann",
+            "right_answer": 4
         },
         {
-            "question": "Mit welchem Tag wird ein Link bestimmt?",
-            "answer_1": "&lti>",
-            "answer_2": "&lta>",
-            "answer_3": "&ltb>",
-            "answer_4": "&ltg>",
+            "question": "Was ist ein string?",
+            "answer_1": "Ein String dient dem Vergleichen von Argumenten",
+            "answer_2": "Ein String beschreibt eine Abfolge von Befehlen",
+            "answer_3": "Ein String denfiniert eine Zahl",
+            "answer_4": "Ein String ist eine Zeichenkette",
+            "right_answer": 4
+        },{
+            "question": "Wie bindet man eine Java Script Datei ein?",
+            "answer_1": "Mit dem a Tag",
+            "answer_2": "Mit dem script Tag",
+            "answer_3": "mit dem i Tag",
+            "answer_4": "Mit dem js Tag",
+            "right_answer": 2
+        },{
+            "question": "In welchem Fall ist a nicht gleich b?",
+            "answer_1": "a > b",
+            "answer_2": "a != b",
+            "answer_3": "a < b",
+            "answer_4": "a == b",
             "right_answer": 2
         }
     ]
